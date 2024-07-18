9d3473345205c20d5018179230a89a0759374209
Lucas Ribeiro <145038823+lribeiroalves@users.noreply.github.com>
2024-07-18 18:44:29 -0300

    Implemented load extensions

--- PyTradeGames/app.py
+++ PyTradeGames/app.py
@@ -1,19 +1,16 @@
 """Creation and managing of the app, app settings and app factory."""
 
 from flask import Flask
-
 from .ext import configuration
-from .ext import database
-from .ext import commands
-
-app = Flask(__name__)
 
 
-configuration.init_app(app)
-database.init_app(app)
-commands.init_app(app)
+def create_app():
+    app = Flask(__name__)
+    configuration.init_app(app)
+    
+    @app.route('/')
+    def index():
+        return 'Hello, World!'
 
+    return app
 
-@app.route('/')
-def index():
-    return str(app.config.get("TITLE"))
--- PyTradeGames/ext/configuration/__init__.py
+++ PyTradeGames/ext/configuration/__init__.py
@@ -4,4 +4,4 @@ from dynaconf import FlaskDynaconf
 
 
 def init_app(app):
-    FlaskDynaconf(app)
+    FlaskDynaconf(app, extensions_list="EXTENSIONS")
