f627b5d3a778cd462dbd22e99236ebbaa62592d6
Lucas Ribeiro <145038823+lribeiroalves@users.noreply.github.com>
2024-07-18 03:43:31 -0300

    database init app

--- PyTradeGames/app.py
+++ PyTradeGames/app.py
@@ -1,10 +1,14 @@
 from flask import Flask
 
 from .ext import configuration
+from .ext import database
 
 app = Flask(__name__)
 
+
 configuration.init_app(app)
+database.init_app(app)
+
 
 @app.route('/')
 def index():
