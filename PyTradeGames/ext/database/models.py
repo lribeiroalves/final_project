f1ee969344e0abff97ee444b4666a07ea19f6ccc
Lucas Ribeiro <145038823+lribeiroalves@users.noreply.github.com>
2024-07-18 22:55:28 -0300

    Update models.py

--- PyTradeGames/ext/database/models.py
+++ PyTradeGames/ext/database/models.py
@@ -2,7 +2,7 @@
 
 from . import db
 
-from sqlalchemy import String, Integer, DateTime, Boolean, ForeignKey, Column
+from sqlalchemy import String, Integer, DateTime, Boolean, ForeignKey, Column, CheckConstraint
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from typing import Optional
 
@@ -10,7 +10,49 @@ from typing import Optional
 class Users(db.Model):
     id:Mapped[int] = mapped_column(primary_key=True)
     username:Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
+    email:Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
     password:Mapped[str] = mapped_column(String(255), nullable=False)
+    country:Mapped[str] = mapped_column(String(255), nullable=False)
+    # games
 
     def __repr__(self) -> str:
         return f'User(id={self.id}, username={self.username})'
+
+
+class Games(db.Model):
+    id:Mapped[int] = mapped_column(primary_key=True)
+    name:Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
+    # genre
+    # console
+    # users
+
+
+class Genres(db.Model):
+    id:Mapped[int] = mapped_column(primary_key=True)
+    genre:Mapped[str] = mapped_column(String(255), nullable=False, unique=True)
+    # games
+    
+
+class Consoles(db.Model):
+    id:Mapped[int] = mapped_column(primary_key=True)
+    name:Mapped[str] = mapped_column(String(255), unique=True, nullable=True)
+    manufacturer:Mapped[str] = mapped_column(String(255), nullable=False)
+    # games
+    
+
+class Messages(db.Model):
+    id:Mapped[int] = mapped_column(primary_key=True)
+    content:Mapped[str] = mapped_column(String(255), nullable=False)
+    status:Mapped[bool] = mapped_column(nullable=False)
+    # sender
+    # receiver
+    # game
+    
+
+class Review(db.Model):
+    id:Mapped[int] = mapped_column(primary_key=True)
+    grade:Mapped[int] = mapped_column(CheckConstraint('grade > 0 AND grade < 6'), nullable=False)
+    message:Mapped[Optional[str]] = mapped_column(String(255))
+    # user_reviewer
+    # user_reviewed
+    
