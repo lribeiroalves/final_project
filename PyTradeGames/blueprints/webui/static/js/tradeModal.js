41a8ea36b0f5e52a5fa07ae5d599a87f211aa97b
Lucas Ribeiro <145038823+lribeiroalves@users.noreply.github.com>
2024-08-15 23:33:24 -0300

    trade modal creation

---
+++ PyTradeGames/blueprints/webui/static/js/tradeModal.js
@@ -0,0 +1,13 @@
+
+// ------------------------------------------------------------
+// Dynamic modal content
+// ------------------------------------------------------------
+const modal = document.getElementById('confirmationModal')
+
+if (modal) {
+    modal.addEventListener('show.bs.modal', event => {
+        // Button that triggered the modal
+        const button = event.relatedTarget;
+        console.log(button)
+    })
+}
